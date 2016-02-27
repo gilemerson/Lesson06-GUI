de4ed7e0f1c8315b6c1297ff534dd3b570922043
Gil Emerson <emerson-gil@outlook.com>
2016-02-27 18:07:10 -0500

    Added 'GUI' ExampleForm Functions

--- Lesson06-GUI/src/ExampleFrame.java
+++ Lesson06-GUI/src/ExampleFrame.java
@@ -4,22 +4,37 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
 
 public class ExampleFrame extends JFrame {
 
 	//Private Instance Variable
 	private JPanel _contentPanel;
+	private EmptyBorder _myEmptyBorder;
+	private BorderLayout _myBorderLayout;
+	private JLabel _firstNameLabel;
 
 	/**
 	 * Create the frame.
 	 * --------Constructor--------
 	 */
+	//Private Method
 	public ExampleFrame() {
+		_initialize();
+	}
+
+	private void _initialize() {
+		this._myEmptyBorder = new EmptyBorder(5, 5, 5, 5);
+		this._myBorderLayout = new BorderLayout(0, 0);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		this._contentPanel = new JPanel();
-		this._contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this._contentPanel.setLayout(new BorderLayout(0, 0));
+		this._contentPanel.setBorder(this._myEmptyBorder);
+		this._contentPanel.setLayout(this._myBorderLayout);
 		setContentPane(this._contentPanel);
+		
+		this._firstNameLabel = new JLabel("First Name:");
+		this._contentPanel.add(this._firstNameLabel, BorderLayout.NORTH);
 	}
 }
--- Lesson06-GUI/src/Program.java
+++ Lesson06-GUI/src/Program.java
@@ -1,8 +1,13 @@
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class Program {
 
 	public static void main(String[] args) {
-		ExampleFrame frame = new ExampleFrame();
-		frame.setVisible(true);
+		ExampleFrame myFrame = new ExampleFrame();
+		myFrame.setVisible(true);
 	}
+	
 }
